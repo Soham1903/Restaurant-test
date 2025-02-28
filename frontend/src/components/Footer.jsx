1f69760b8dd692959d5a2306485301ca6a137bfc
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2025-02-28 11:21:59 +0530

    Initial Commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">LUNA DINE</div>
+          <div className="right">
+            <p>D.N.Nagar, Mumbai</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By SOHAM LADGAONKAR</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By SOHAM LADGAONKAR.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
